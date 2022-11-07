37cbce0f5975c932bc96e4ccd0c83d3f4bf6806f
changxianglin <281532612@qq.com>
2022-11-08 02:01:34 +0800

    a mini react project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+
+import MovieCard from './MovieCard'
+
+import './App.css'
+
+
+const API_URL = 'http://www.omdbapi.com?apikey=c032e2d7'
+
 
 const App = () => {
+
+  const [movies, setMovies] = useState([])
+
+  const searchMovies = async (title) => {
+
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+
+    setMovies(data.Search)
+  }
+
+  useEffect(() => {
+    searchMovies()
+  }, [])
   return (
-    <h1>App</h1>
+    <div className='app'>
+      <h1>MovieLand</h1>
+
+      <div className='search'>
+        <input
+          placeholder=''
+          value='Superman'
+          onChange={() => {}}
+        />
+        <img 
+          src={'SearchIcon'}
+          alt="search"
+          onClick={() => {}}
+        />
+      </div>
+
+    {
+      movies.length > 0 ? 
+      (
+        <div className='container'>
+        {
+          movies.map((item, index) => (
+            <MovieCard movie1={item} key={index} />
+          ))
+        }
+      </div>
+      ): (
+        <div>
+          <h2>No movies found</h2>
+        </div>
+      )
+    }
+    </div>
   )
 }
 
